9d68ee6ab61706a18a1c1d00fa9bb8081b8a99ad
EL Wauga <55194955+benghi254@users.noreply.github.com>
2026-04-08 09:27:11 +0300

    changes

--- user/newTrash.php
+++ user/newTrash.php
@@ -1,13 +1,11 @@
 <?php
-
+include_once '../commons/auth.php';
 
 if(isset($_POST['weight'],$_POST['collectDay'],$_POST['area'],$_POST['details'],$_POST['trashType'],$_POST['userId'])
 && !empty($_POST['weight']) && !empty($_POST['collectDay']) && !empty($_POST['area'])&& !empty($_POST['details']))
 {
     include_once 'trash.php';
     include_once 'Ftrash.php';
-    
-
     
     $trash=new Trash(null,$_POST['weight'],$_POST['collectDay'],$_POST['area'],$_POST['details'],$_POST['trashType'],$_POST['userId'],null);
  
@@ -16,11 +14,15 @@ if(isset($_POST['weight'],$_POST['collectDay'],$_POST['area'],$_POST['details'],
     {
         $_SESSION['err']="Impossible to add this address";
         header('Location: location.php');
+        exit;
     }else{
-        
         $_SESSION['done']="Trash added Successfully";
- 
-        
         header('Location: checkout.php');
+        exit;
     }
-}?>
+} else {
+    $_SESSION['err'] = "Please fill in all the required fields, including location details. Make sure you set your address properly.";
+    header('Location: start.php');
+    exit;
+}
+?>
